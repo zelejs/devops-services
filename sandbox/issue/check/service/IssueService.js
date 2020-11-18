19295e5daf1d1e4d98d3bce8088a878534e1d024
zxchengb <zxchengb@163.com>
2020-11-18 16:33:59 +0800

    Update distinct insert function in check api.

--- sandbox/issue/check/service/IssueService.js
+++ sandbox/issue/check/service/IssueService.js
@@ -4,18 +4,20 @@ var connection = mysql.createConnection(dbInfo);
 var table = "cr_issue_task";
 
 module.exports = {
-  addIssue: function(issue) {
+  addIssue: async function(issue) {
     // var addSql = 'INSERT INTO ' + table + ' (title,brief,module_name,status,type,priority) VALUES(?,?,?,?,?,?)';
-    var addSql = 'IF NOT EXITIS (SELECT * FROM '+ table +' WHERE brief=? AND module_name=?) THEN INSERT INTO ' + table + ' SET ?';
-    console.log(issue);
-    connection.query(addSql, [issue.brief, issue.module_name, issue], function (err, result) {
-      if(err){
-        console.log('[INSERT ERROR] - ',err.message);
-        return;
-      }        
-      console.log('--------------------------INSERT----------------------------');
-      console.log('INSERT ID:',result);        
-      console.log('-----------------------------------------------------------------\n\n');  
+    var addSql = 'INSERT INTO ' + table + ' SET ?';
+    var querySql = 'SELECT count(*) from '+ table +' where brief = ? and module_name = ?';
+    connection.query(querySql, [issue.brief, issue.module_name],(err, results) => {
+      if(results.length <= 0){
+        connection.query(addSql, issue, function (err, result) {
+          if(err){
+            console.log('[INSERT ERROR] - ',err.message);
+            return;
+          }        
+          console.log('INSERT ID:',result);        
+        });
+      } 
     });
   }
 }
